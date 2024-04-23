ff642ef3508a856ff9fade416bb7a10357a0220b
ShauryaSambyal <shauryasambyal12e4@gmail.com>
2024-04-23 21:08:28 +0530

    Cuberto Clone

---
+++ shery.js
@@ -0,0 +1,35 @@
+Shery.mouseFollower()
+Shery.makeMagnet('.magnet')
+Shery.hoverWithMediaCircle('.hvr', {images: ['images/1.jpg', 'images/2.jpg', 'images/3.png']})
+
+
+gsap.to('.fleftelem', {
+    scrollTrigger: {
+        trigger: '.fimages',
+        pin: true,
+        start: 'top top',
+        end: 'bottom bottom',
+        endTrigger: '.last',
+        scrub: 1
+    },
+    y: '-300%',
+    ease: Power1
+})
+
+let sections = document.querySelectorAll('.fleftelem')
+Shery.imageEffect(".images", {
+    style: 5,
+    config: {onMouse: {value: 1}},
+    slideStyle: (setScroll) => {
+        sections.forEach(function(section, index) {
+            ScrollTrigger.create({
+                trigger: section,
+                start: 'top top',
+                scrub: 1,
+                onUpdate: function(prog) {
+                    setScroll(prog.progress+index)
+                }
+            })
+        })    
+    },
+  });
